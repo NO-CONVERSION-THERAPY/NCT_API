70f575b0292871e83efca2480b3ba724d26edd46
星野栄治 <177436503+HosinoEJ@users.noreply.github.com>
2026-04-22 20:40:19 +0800

    Update app.js

--- app/app.js
+++ app/app.js
@@ -7,31 +7,23 @@ app.get('/api/map-data', async (c) => {
     try {
         const response = await fetch(gasUrl);
         
-        if (!response.ok) {
-            throw new Error(`GAS get err: ${response.status}`);
-        }
-        
         const data = await response.json();
 
         const cleanData = data.map(item => {
             return {
                 name: item.name,
-                statistics: item.statistics,
-                data: {
-                lat: item.lat,
-                lng: item.lng
-                }
+                statistics: item.statistics
             };
         });
         
         return c.json({
             success: true,
-            content: data,
+            content: cleanData,
             api_by: "HosinoNeko"
         });
         }
     catch (error) {
-        return c.status(500).json({ error: error.message });
+        return c.json({ error: error.message }, 500);
     }
 });
 
