25e4443ad2e59cc85d397835ce3f3a63855a2360
星野栄治 <177436503+HosinoEJ@users.noreply.github.com>
2026-04-22 18:07:16 +0800

    c

--- app/app.js
+++ app/app.js
@@ -1,25 +1,26 @@
 import { Hono } from 'hono';
 const app = new Hono();
 
-app.get('/api/map-data', async (req, res) => {
+app.get('/api/map-data', async (c) => {
   const gasUrl = c.env.GAS_URL;
   
-  try {
-    const response = await fetch(gasUrl);
-    
-    if (!response.ok) {
-      throw new Error(`GAS get err: ${response.status}`);
+    try {
+        const response = await fetch(gasUrl);
+        
+        if (!response.ok) {
+            throw new Error(`GAS get err: ${response.status}`);
+        }
+        
+        const data = await response.json();
+        
+        return c.json({
+            content: data,
+            api_by: "HosinoNeko"
+        });
+        }
+    catch (error) {
+        return c.status(500).json({ error: error.message });
     }
-    
-    const data = await response.json();
-    
-    res.json({
-      content: data,
-      api_by: "HosinoNeko"
-    });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
 });
 
 export default app;
