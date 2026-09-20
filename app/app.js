efe246d63d83b7fda56bf74b7ebd8607c9ecafe4
HosinoNeko <177436503+HosinoEJ@users.noreply.github.com>
2026-09-20 11:35:05 +0800

    fix:暂时取消缓存

--- app/app.js
+++ app/app.js
@@ -51,10 +51,11 @@ app.get('/api/map-data', async (c) => {
             data: cleanDataForData,
 
             api_by: `HosinoNeko\n在没有黑暗的地方 我们终将重逢`
-        }, 200 , {
-            'Cache-Control': 'public, max-age=300'
-        });
-        }
+        }//, 200 , {
+        //    'Cache-Control': 'public, max-age=300'
+        //}
+        );
+    }
     catch (error) {
         return c.json({ error: error.message }, 500);
     }
